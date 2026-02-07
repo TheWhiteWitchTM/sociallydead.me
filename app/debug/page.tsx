56eeeb6f3823798dd3e17d215ba2a29c30956e81
🧙‍♀️𝕿𝖍𝖊 𝖂𝖍𝖎𝖙𝖈𝖍™✨ <thewhitewitchtm@gmail.com>
2026-02-07 03:53:51 +0100

    Debug

--- app/debug/page.tsx
+++ app/debug/page.tsx
@@ -1,7 +1,21 @@
+"use client"
+
+import {Bug } from "lucide-react";
+import {useBluesky} from "@/lib/bluesky-context";
+
 export default function Debug() {
+	const blueSky = useBluesky();
+	const agent = blueSky.agent;
 	return(
-		<div>
-			Debug
+		<div className="min-h-screen">
+			<header className="sticky top-0 z-30 border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+				<div className="flex h-14 items-center justify-between px-4">
+					<div className="flex items-center gap-2">
+						<Bug className="h-5 w-5" />
+						<h1 className="text-xl font-bold">Lists</h1>
+					</div>
+				</div>
+			</header>
 		</div>
 	)
 }
