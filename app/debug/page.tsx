e56d1383ee79702bed87421c6e2251baaebf5486
🧙‍♀️𝕿𝖍𝖊 𝖂𝖍𝖎𝖙𝖈𝖍™✨ <thewhitewitchtm@gmail.com>
2026-02-07 13:39:45 +0100

    Debug

--- app/debug/page.tsx
+++ app/debug/page.tsx
@@ -1,34 +1,13 @@
 "use client"
 
-import {Bug } from "lucide-react";
-import {useBluesky} from "@/lib/bluesky-context";
-import {createSociallyDeadRecord, getSociallyDeadRecord} from "@/lib/sociallydead-me";
+import {useState} from "react";
+import {Bug} from "lucide-react";
 
 export default function Debug() {
-	const blueSky = useBluesky();
-	const agent = blueSky.agent;
-	const user = blueSky.user
-	let record = null;
-	let create = null;
-	if (agent) {
-		const json = {
-			lexicon: 1,
-			label: "user",
-			joined: new Date(),
-			visits: 1,
-			payment: false,
-			props: [],
-			highlights: [],
-			articles: [],
-		}
-		create = createSociallyDeadRecord(agent,json)
-			.then((res) => {return res?.cid})
-			.catch((err) => {return "Error setting record!"})
-		record = getSociallyDeadRecord(agent)
-			.then((res) => {return res?.value})
-			.catch((err) => {return "Error getting record!"})
-	}
-	const out = JSON.stringify(record)
+	const [record, setRecord] = useState("No record!");
+	const [created, setCreated] = useState("Not created!");
+
+
 	return(
 		<div className="min-h-screen">
 			<header className="sticky top-0 z-30 border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
@@ -40,18 +19,17 @@ export default function Debug() {
 				</div>
 			</header>
 			<div>
-				{agent?.did}<br/>
 				<p>
 					{record ? "Record found" : "No record found!"}
 					{record &&
 						<>
 						<h2>sociallydead.me record:</h2>
-						{out}
+						{record}
 						</>
 					}
-					{create &&
+					{created &&
 						<>
-							Created: {create}
+							Created: {created}
 						</>
 					}
 				</p>
